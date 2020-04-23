50ea8710d6aa20ce0626978df067b68984a52ce7
박응용 <pahkey@gmail.com>
2020-04-23 22:03:33 +0900

    logging

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -1,12 +1,17 @@
+import logging
+
 from django.core.paginator import Paginator
 from django.db.models import Q, Count
 from django.shortcuts import render, get_object_or_404
 
 from ..models import Question
 
+logger = logging.getLogger('pybo')
+
 
 def index(request):
-    3/0
+    logger.info("INFO 레벨로 출력")
+
     """
     pybo 목록출력
     """
