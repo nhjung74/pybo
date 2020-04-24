85091186ea476055151a0fabd09a0852dcec4ca0
박응용 <pahkey@gmail.com>
2020-04-24 13:53:54 +0900

    postgresql

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,3 +1,6 @@
 from .base import *
 
 ALLOWED_HOSTS = []
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,16 +1,16 @@
 from .base import *
 
 ALLOWED_HOSTS = ['52.78.8.100', 'pybo.kr']
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 DEBUG = False
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'pybo',
-        'USER': 'pybo',
-        'PASSWORD': 'v[E}0dWWYj*EDh+5ivKe~Zult0mG&E8&',
-        'HOST': 'ls-e4fc508cb81e4d605579d0d07ce9344d54cb552d.cqlcyugj7ibs.ap-northeast-2.rds.amazonaws.com',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'pybo',
+#         'USER': 'pybo',
+#         'PASSWORD': 'v[E}0dWWYj*EDh+5ivKe~Zult0mG&E8&',
+#         'HOST': 'ls-e4fc508cb81e4d605579d0d07ce9344d54cb552d.cqlcyugj7ibs.ap-northeast-2.rds.amazonaws.com',
+#         'PORT': '5432',
+#     }
+# }
