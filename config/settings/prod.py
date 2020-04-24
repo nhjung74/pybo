89d163e8d51b995d29806cdc0cb3bf24404e73b4
박응용 <pahkey@gmail.com>
2020-04-24 13:20:07 +0900

    postgresql

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -7,7 +7,7 @@ DEBUG = False
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'Database-1',
+        'NAME': 'pybo',
         'USER': 'pybo',
         'PASSWORD': 'v[E}0dWWYj*EDh+5ivKe~Zult0mG&E8&',
         'HOST': 'ls-e4fc508cb81e4d605579d0d07ce9344d54cb552d.cqlcyugj7ibs.ap-northeast-2.rds.amazonaws.com',
