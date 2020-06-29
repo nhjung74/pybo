6a56db6b2eda6d626086bff9d57a150665df2ead
Ubuntu <ubuntu@ip-172-26-12-247.ap-northeast-2.compute.internal>
2020-06-29 15:05:52 +0900

    domain changed

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['52.78.8.100', 'pybo.kr']
+ALLOWED_HOSTS = ['52.78.8.100', 'django.pybo.kr']
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATICFILES_DIRS = []
 DEBUG = False
